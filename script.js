32e9ab899c8b495bd9e107b5543f40646d79f69d
CharlotteSoer <111729@emmauscollege.nl>
2020-09-09 10:20:38 +0000

    Bal kaatst

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
+var balX = 50;
+var balY = 50;
+var speedX = 2;
+var speedY = 2;
+
 /**
  * setup
  * de code in deze functie wordt één keer uitgevoerd door
  * de p5 library, zodra het spel geladen is in de browser
  */
+
 function setup() {
   // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
   createCanvas(1280, 720);
-
-  // Kleur de achtergrond blauw, zodat je het kunt zien
-  background('blue');
 }
-
 
 /**
  * draw
  * de code in deze functie wordt meerdere keren per seconde
  * uitgevoerd door de p5 library, nadat de setup functie klaar is
  */
+
 function draw() {
+   // maak de achtergrond blauw
+   background('blue');
+
   // stel vulkleur in
-  fill(100, 100, 255);
+  fill(255, 100, 255);
 
   // teken een cirkel
-  ellipse(50,50,80,80);
+  ellipse(balX,balY,80,80);
+
+  // beweeg de bal diagonaal
+  balX = balX + speedX;
+  balY = balY + speedY;
+
+  // voorwaarden stellen voor randen
+  if(balX => 1240) {
+      speedX = speedX * -1;
+  }
+
+  if(balY => 680) {
+      speedY = speedY * -1;
+  }
+
+  if(balX =< 40) {
+      speedX = speedX * 1;
+  }
+
+  if(balY =< 40) {
+      speedY = speedY * 1;
+  }
 }
